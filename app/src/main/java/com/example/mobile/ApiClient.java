6e4a4215306c2a5fe455e346df93d9752b96bb57
Aryaa140 <aryaadaffaj@gmail.com>
2025-09-19 08:19:37 +0700

    Ars

--- app/src/main/java/com/example/mobile/ApiClient.java
+++ app/src/main/java/com/example/mobile/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.2.102/quality_mobile_api/";
+    private static final String BASE_URL = "http://192.168.2.103/quality_mobile_api/";
     // pakai 10.0.2.2 kalau di emulator (localhost)
     // kalau hosting: "https://domainkamu.com/api/"
 
